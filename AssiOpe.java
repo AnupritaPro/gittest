c1f501688d992091f72d713e0d0d9bd51ae8b4c4
AnupritaPro <anupritarajput@gmail.com>
2020-12-17 13:37:44 +0530

    i added AssiOpe.java

---
+++ AssiOpe.java
@@ -0,0 +1,25 @@
+//Assigment Operator
+
+class AssiOpe{
+public static void main(String args[]){
+	int a=24,b=2;
+
+	a+=b;				//a=a+b
+	System.out.println("Addition of number:\t"+a);
+
+	a-=b;				//a=a-b
+	System.out.println("Subtraction of number\t"+a);
+
+	a*=b;				//a=a*b
+	System.out.println("Multiplication of number:\t"+a);
+
+	a/=b;				//a=a/b
+	System.out.println("Division of number:\t"+a);
+
+	a%=b;				//a=a%b
+	System.out.println("Mode of number:\t"+a);
+
+	
+
+	}
+}
