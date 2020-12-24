5135f76b283f0c45c415917aca2677a4b74fd664
AnupritaPro <anupritarajput@gmail.com>
2020-12-24 20:25:46 +0530

    directory program

---
+++ dir1.py
@@ -0,0 +1,13 @@
+
+dir1={'Name':'Anu','Age':'20'}
+dir2={'Name':'Anu','Age':'20'}
+dir3={'Name':'Anu','Age':'22'}
+dir4={'Name':'Sar','Age':'24'}
+
+print "Return value:%d" % cmp(dir1,dir2)
+print "Return value :%d"% cmp(dir1,dir3)
+print "Return value :%d" % cmp(dir1,dir4)
+
+
+print "Length :%d" % len(dir1)
+
