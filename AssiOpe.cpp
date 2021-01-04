1a9f2e4f0e7ef3e2b73e930423db0aa0ce315adf
AnupritaPro <anupritarajput@gmail.com>
2021-01-04 22:03:32 +0530

    arithmatic

---
+++ AssiOpe.cpp
@@ -0,0 +1,23 @@
+//Assigment Operator
+
+#include<iostream>
+using namespace std;
+int main(){
+	int b=2,c=4;        
+	c+=b;                 //Adding
+	cout<<"Addition of number is:"<<c<<endl;
+	
+	c-=b;			//Subtracting
+	cout<<"subtraction of number is:"<<c<<endl;
+
+	c*=b;			//Multiplication
+	cout<<"multiplication of number is "<<c<<endl;
+
+	c/=b;			//Division
+	cout<<"Division of number "<<c<<endl;
+
+	c%=b;			//Module
+	cout<<"module of number is "<<c<<endl;
+
+}
+
