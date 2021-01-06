82f9e9d0c3463a4af37d1d8948f50c21493d4876
AnupritaPro <anupritarajput@gmail.com>
2021-01-06 08:38:44 +0530

    Relational operator

---
+++ RelOpe.cpp
@@ -0,0 +1,21 @@
+//Relational Operator
+
+#include<iostream>
+using namespace std;
+int main(){
+
+	int a=20,b=20;
+	
+
+	if(a==b	)			//equal
+	cout<<"a is equal to b"<<endl;
+
+	else if(a<b)			//less than operator
+	cout<<"a is not less than b"<<endl;
+
+	else if (a>b)			//greater than operator
+	cout<<"a is greater than b"<<endl;
+
+	else if (a!=b)			//not equal
+	cout<<"a is not equal to b"<<endl;
+}
